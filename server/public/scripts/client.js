47941fd3cd00678bdd941287718a59bd04427357
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-02-27 15:40:21 -0600

    refactoring

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,59 +1,51 @@
 $(document).ready(function() {
   console.log('jquery loaded');
 
-//get jokes from server
+  //get jokes from server
   $.ajax({
     type: 'GET',
     url: '/jokes',
     success: function(response){
       console.log('back from ajax with: ', response);
-      for (var i = 0; i < response.length; i++) {
-        console.log(response[i]);
-        $('#jokeBook').append(
-          '<div class ="joke">' +
-          '<h2>' + response[i].whoseJoke + '\'s Joke</h2>' +
-          '<p>' + response[i].jokeQuestion + '</p>' +
-          '<p>' + response[i].punchLine + '</p>' +
-        '</div>'
-        );
-      }
+      appendingJokes(response);
     }//end success
   });//end ajax get
 
-$('button').on('click', function(){
-  //create an object from inputs
-  var newJoke = $('#joke').val();
-  var userName = $('#name').val();
-  var newPunch = $('#punchLine').val();
-console.log('almost and object: ', newJoke, userName, newPunch);
+  $('button').on('click', function(){
+    //create an object from inputs
+    var newJoke = $('#joke').val();
+    var userName = $('#name').val();
+    var newPunch = $('#punchLine').val();
+    console.log('almost and object: ', newJoke, userName, newPunch);
+    var jokeObject = {
+      whoseJoke: userName,
+      jokeQuestion: newJoke,
+      punchLine: newPunch
+    };
+    // post new joke to server on submit
+    $.ajax({
+      type: 'POST',
+      url: '/newJokes',
+      data: jokeObject,
+      success: function(response){
+        console.log('post response: ', response);
+        $('#jokeBook').empty();
+        appendingJokes(response);
+      }//ends success
+    });//end ajax post
+  });//end button click
 
-var jokeObject = {
-  whoseJoke: userName,
-  jokeQuestion: newJoke,
-  punchLine: newPunch
-};
-//
-// post new joke to server on submit
-  $.ajax({
-    type: 'POST',
-    url: '/newJokes',
-    data: jokeObject,
-    success: function(response){
-      console.log('post response: ', response);
-      $('#jokeBook').empty();
-      for (var i = 0; i < response.length; i++) {
-        console.log(response[i]);
-        $('#jokeBook').append(
-          '<div class ="joke">' +
-          '<h2>' + response[i].whoseJoke + '\'s Joke</h2>' +
-          '<p>' + response[i].jokeQuestion + '</p>' +
-          '<p>' + response[i].punchLine + '</p>' +
+  function appendingJokes(response){
+    for (var i = 0; i < response.length; i++) {
+      console.log(response[i]);
+      $('#jokeBook').append(
+        '<div class ="joke">' +
+        '<h2>' + response[i].whoseJoke + '\'s Joke</h2>' +
+        '<p>' + response[i].jokeQuestion + '</p>' +
+        '<p>' + response[i].punchLine + '</p>' +
         '</div>'
-        );
-      }
-    }//ends success
-  });//end ajax post
-
-});//end button click
+      );
+    }
+  }
 
 });//end doc ready
